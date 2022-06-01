228eccb8cbb85d6aaebdaca813311f65d6ae899d
toh995 <toh995@gmail.com>
2022-05-31 22:07:20 -0700

    acronym

--- exercises/practice/acronym/acronym.sh
+++ exercises/practice/acronym/acronym.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
+main() {
+  local -ar words=$@
+  local ret=""
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+  for word in ${words}; do
+    ret+="${word:0:1}"
+  done
+
+  echo "${ret}"
+}
+main $@
