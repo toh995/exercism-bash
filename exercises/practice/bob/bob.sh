b8c9070a12d0844c2328e8396eabd633e88732ce
toh995 <toh995@gmail.com>
2022-06-01 01:11:12 -0700

    bob

--- exercises/practice/bob/bob.sh
+++ exercises/practice/bob/bob.sh
@@ -1,24 +1,53 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+is_question() {
+  # remove all trailing whitespace from the input
+  local -r input="$(echo -e "${1}" | sed -e 's/[[:space:]]*$//')"
+  if [[ "${input: -1}" == "?" ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+is_all_caps() {
+  local -r input="${1}"
+  # must have at least one capital
+  if [[ "${input}" =~ [A-Z] ]] \
+    && [[ "${input}" =~ ^[^a-z]*$ ]] # cannot have lowercase
+  then 
+    return 0
+  else
+    return 1
+  fi
+}
+
+is_empty() {
+  local -r input="${1}"
+  # match empty/unset string
+  if [[ -z "${input}" ]] \
+    || [[ "${input}" =~ ^[[:space:]]+$ ]] # match \t, \n, \r, etc.
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
+main() {
+  local -r input="${1}"
+  if is_question "${input}"; then
+    if is_all_caps "${input}"; then
+      echo "Calm down, I know what I'm doing!"
+    else
+      echo "Sure."
+    fi
+  elif is_all_caps "${input}"; then
+    echo "Whoa, chill out!"
+  elif is_empty "${input}"; then
+    echo "Fine. Be that way!"
+  else
+    echo "Whatever."
+  fi
+}
+main "${@}"
