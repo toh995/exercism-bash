14188f49f698820bdbca494ba9aec70cc5ef5ce2
toh995 <toh995@gmail.com>
2022-05-31 21:43:34 -0700

    raindrops

--- exercises/practice/raindrops/raindrops.sh
+++ exercises/practice/raindrops/raindrops.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+raindrops() {
+  local og_number="${1}"
+  local ret=""
+
+  if [[ $((og_number % 3)) == 0 ]]; then
+    ret+="Pling"
+  fi
+  if [[ $((og_number % 5)) == 0 ]]; then
+    ret+="Plang"
+  fi
+  if [[ $((og_number % 7)) == 0 ]]; then
+    ret+="Plong"
+  fi
+  if [[ "${ret}" == "" ]]; then
+    ret="${og_number}"
+  fi
+
+  echo "${ret}"
+}
+raindrops $@
