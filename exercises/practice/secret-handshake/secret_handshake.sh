e0867f92569affd9bae2956aefb364f62bb494d0
toh995 <toh995@gmail.com>
2022-06-02 03:08:08 -0700

    secret-handshake

--- exercises/practice/secret-handshake/secret_handshake.sh
+++ exercises/practice/secret-handshake/secret_handshake.sh
@@ -1,24 +1,48 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+reverse_array_in_place() {
+  local -n arr="${1}" # store a reference to the original array
+  local -ir len="${2}"
+  local -i i
+  local tmp
+
+  for (( i=0; i < ("${len}"/2); i++ )); do
+    tmp="${arr[i]}"
+    arr[i]="${arr[-1-i]}"
+    arr[-1-i]="${tmp}"
+  done
+}
+
+main() {
+  local -ir binary="$(bc <<< "obase=2;${1}")"
+  local -a instructions=()
+  local -i instruction_len=0
+  local ret=""
+
+  # build the instructions array
+  (( "${#binary}" >= 1 )) && (( "${binary: -1:1}" == 1 )) && \
+    instructions+=("wink") && \
+    let instruction_len+=1
+  (( "${#binary}" >= 2 )) && (( "${binary: -2:1}" == 1 )) && \
+    instructions+=("double blink") && \
+    let instruction_len+=1
+  (( "${#binary}" >= 3 )) && (( "${binary: -3:1}" == 1 )) && \
+    instructions+=("close your eyes") && \
+    let instruction_len+=1
+  (( "${#binary}" >= 4 )) && (( "${binary: -4:1}" == 1 )) && \
+    instructions+=("jump") && \
+    let instruction_len+=1
+  (( "${#binary}" >= 5 )) && (("${binary: -5:1}" == 1 )) && \
+    reverse_array_in_place instructions instruction_len
+
+  # serialize the instructions for output
+  for instruction in "${instructions[@]}"; do
+    ret+="${instruction},"
+  done
+
+  (( "${#ret}" > 0 )) && \
+    ret="${ret:0:-1}" # remove the final trailing comma
+
+  echo "${ret}"
+}
+main "${@}"
