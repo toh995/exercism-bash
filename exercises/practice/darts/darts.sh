b10df875f28a1a06e4730214fcdb226fcddb2b1d
toh995 <toh995@gmail.com>
2022-06-02 19:27:33 -0700

    darts

--- exercises/practice/darts/darts.sh
+++ exercises/practice/darts/darts.sh
@@ -1,24 +1,43 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+readonly INNER_RADIUS=1 INNER_PTS=10
+readonly MIDDLE_RADIUS=5 MIDDLE_PTS=5
+readonly OUTER_RADIUS=10 OUTER_PTS=1
+
+is_inner_circle() {
+  local -r distance="${1}"
+  if [[ "$(bc <<< "${distance} <= ${INNER_RADIUS}")" == "1" ]]; then return 0; else return 1; fi
+}
+is_middle_circle() {
+  local -r distance="${1}"
+  if [[ "$(bc <<< "${distance} <= ${MIDDLE_RADIUS}")" == "1" ]]; then return 0; else return 1; fi
+}
+is_outer_circle() {
+  local -r distance="${1}"
+  if [[ "$(bc <<< "${distance} <= ${OUTER_RADIUS}")" == "1" ]]; then return 0; else return 1; fi
+}
+
+main() {
+  local -r x="${1}"
+  local -r y="${2}"
+  local -r distance="$(bc -l <<< "sqrt( (${x})^2 + (${y})^2 )")"
+  local -i ret
+
+  if [[ ! "${x}" =~ ^-{0,1}[0-9.]+$ || ! "${y}" =~ ^-{0,1}[0-9.]+$ ]]; then
+    echo "need to supply two numbers"
+    exit 1
+  fi
+
+  if is_inner_circle "${distance}"; then
+    ret="${INNER_PTS}"
+  elif is_middle_circle "${distance}"; then
+    ret="${MIDDLE_PTS}"
+  elif is_outer_circle "${distance}"; then
+    ret="${OUTER_PTS}"
+  else
+    ret=0
+  fi
+
+  echo "${ret}"
+}
+main "${@}"
